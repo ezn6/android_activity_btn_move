a39571f5a1a14343e6dc2296d7beb50843139043
ezn6 <kcourtney@naver.com>
2021-05-21 00:26:44 +0900

    Initial commit

---
+++ app/src/main/java/myapplication/ex/MainActivity.java
@@ -0,0 +1,30 @@
+package myapplication.ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /////////////
+        btn_move = findViewById(R.id.btn_move); //xml에서 id찾아연결
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);//액티비티 이동
+            }
+        });
+
+
+    }
+}
